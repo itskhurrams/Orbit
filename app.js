d6b821527008b47fcef9e44149195787a1777846
khurram shahzad <itskhurrams@outlook.com>
2022-03-23 12:48:16 +0500

    error management middleware added

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ const app = express();
 
 app.use('/api/comments', commentsRoutes);
 
+app.use((error, request, response, next) => {
+  if (response.headerSent) {
+    return next(error);
+  }
+  response.status(error.code || 500);
+  response.json(error.message || 'Unknow error occurred !');
+});
+
 app.listen(5000, () => {
   console.log('Server listening at 5000');
 });
--- routes/comments-routes.js
+++ routes/comments-routes.js
@@ -1,12 +1,23 @@
+const res = require('express/lib/response');
+
 const router = require('express').Router();
 
 const DUMMY_USER_COMMENTS = [
   {
     id: '1',
     userId: '0x66155E24bA0EB4aAa5f209F842260AD718f032fD',
-    displayName: 'Khurram Shahzad',
+    displayName: 'Khurram',
     companyId: '0x66155E24bA0EB4aAa5f209F842260AD718f032fD',
     comment: 'I have worked around 4 years and it was good learning experience',
+    createdBy: '0x66155E24bA0EB4aAa5f209F842260AD718f032fD',
+    creatdDate: '03/23/2022 7:11 AM',
+  },
+  {
+    id: '2',
+    userId: '0x66155E24bA0EB4aAa5f209F842260AD718f032fD',
+    displayName: 'Shahzad',
+    companyId: '0x66155E24bA0EB4aAa5f209F842260AD718f032fD',
+    comment: 'Second Comment',
     createdBy: '0x66155E24bA0EB4aAa5f209F842260AD718f032fD',
     creatdDate: '03/23/2022 7:11 AM',
   },
@@ -16,6 +27,24 @@ router.get('/:commentId', (request, response, next) => {
   const comment = DUMMY_USER_COMMENTS.find((comment) => {
     return comment.id === commentId;
   });
+  if (!comment) {
+    const error = new Error('Could not find comment against this Id');
+    error.code = 404;
+    return next(error);
+  }
+
+  response.json({ comment });
+});
+router.get('/user/:userId', (request, response, next) => {
+  const userId = request.params.userId;
+  const comment = DUMMY_USER_COMMENTS.filter((comment) => {
+    return comment.userId === userId;
+  });
+  if (!comment || comment.length == 0) {
+    const error = new Error('No comments found against this User Id');
+    error.code = 404;
+    return next(error);
+  }
   response.json({ comment });
 });
 module.exports = router;
