74a606f59ca7d4b168891c0ec24069f5f8d8d16d
khurram shahzad <itskhurrams@outlook.com>
2022-04-09 10:55:40 +0500

    minus Password

--- controllers/auth-conroller.js
+++ controllers/auth-conroller.js
@@ -1,6 +1,8 @@
 const User = require('../models/user');
 
 const getAuthenticatedUsers = async (request, response, next) => {
-  response.json({ user: await User.findById(request.user.Id) });
+  response.json({
+    user: await User.findById(request.user.Id).select('-passcode'),
+  });
 };
 exports.getAuthenticatedUsers = getAuthenticatedUsers;
