cd76cff042597481306dde610afec7958b73995b
khurram shahzad <itskhurrams@outlook.com>
2022-04-09 14:37:41 +0500

    user model also update

--- models/user.js
+++ models/user.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose');
 const mongooseUniqueValidator = require('mongoose-unique-validator');
 
 const userSchema = new mongoose.Schema({
-  displayName: { type: String, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  title: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   passcode: { type: String, required: true, minlength: 6 },
   avatar: { type: String },
   createdDate: { type: Date, default: Date.now },
-  // image: { type: String, required: true },
-  // createdBy: { type: String, required: true },
 });
 userSchema.plugin(mongooseUniqueValidator);
 module.exports = mongoose.model('User', userSchema);
