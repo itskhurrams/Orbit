f33e3edc80d83501dc48c92730dd2db6983172a8
khurram shahzad <itskhurrams@outlook.com>
2022-04-19 16:23:40 +0500

    cleanup with structure done

--- client/src/views/home.jsx
+++ client/src/views/home.jsx
@@ -1,9 +1,9 @@
-import './assets/styles/index.css';
+import '../assets/styles/index.css';
 
-const App = () => (
+const Home = () => (
   <div>
     <h1 className='text-3xl font-bold underline'>Client App</h1>
   </div>
 );
 
-export default App;
+export default Home;
