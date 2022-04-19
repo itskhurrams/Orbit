eb3ea99845ae82d0ae7692f1c582c2b77ef08618
khurram shahzad <itskhurrams@outlook.com>
2022-04-19 16:40:33 +0500

    some renames

--- client/src/views/App.jsx
+++ client/src/views/App.jsx
@@ -1,9 +1,9 @@
 import '../assets/styles/index.css';
 
-const Home = () => (
+const App = () => (
   <div>
     <h1 className='text-3xl font-bold underline'>Client App</h1>
   </div>
 );
 
-export default Home;
+export default App;
