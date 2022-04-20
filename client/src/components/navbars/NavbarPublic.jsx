ecb580ace0bd5d5dcd5c68d7cf7f8334f166a02e
khurram shahzad <itskhurrams@outlook.com>
2022-04-20 15:12:10 +0500

    cleanup

--- client/src/components/navbars/NavbarPublic.jsx
+++ client/src/components/navbars/NavbarPublic.jsx
@@ -13,9 +13,7 @@ const NavbarPublic = (props) => {
             className='text-blueGray-700 text-xl font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap tracking-widest'
           >
             W E L T Y ~{' '}
-            <span className='text-xs lowercase font-normal'>
-              your voice matters
-            </span>
+            <span className='text-xs font-normal'>your voice matters</span>
           </Link>
           <button
             className='cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none'
