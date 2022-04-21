cfc2ff0cd38a2c22525d5b4257ad4a8b332cc98a
khurram shahzad <itskhurrams@outlook.com>
2022-04-21 11:53:10 +0500

    signup company done

--- client/src/views/auth/CompanySignup.jsx
+++ client/src/views/auth/CompanySignup.jsx
@@ -22,15 +22,13 @@ const CompanySignup = (props) => {
                     className='bg-white active:bg-blueGray-50 text-blueGray-700 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-2 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150'
                     type='button'
                   >
-                    <img alt='...' className='w-5 mr-1' src='' />
-                    Github
+                    <i className='fab fa-facebook-square text-5xl'></i>
                   </button>
                   <button
                     className='bg-white active:bg-blueGray-50 text-blueGray-700 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-1 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150'
                     type='button'
                   >
-                    <img alt='...' className='w-5 mr-1' src='' />
-                    Google
+                    <i className='fa-brands fa-google text-5xl'></i>
                   </button>
                 </div>
                 <hr className='mt-6 border-b-1 border-blueGray-300' />
