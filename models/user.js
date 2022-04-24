6c348c3471ff11b0639df8986e81f771e5e19244
khurram shahzad <itskhurrams@outlook.com>
2022-04-24 20:54:16 +0500

    user model update

--- models/user.js
+++ models/user.js
@@ -2,20 +2,14 @@ const mongoose = require('mongoose');
 const mongooseUniqueValidator = require('mongoose-unique-validator');
 
 const userSchema = new mongoose.Schema({
+  companyName: { type: String },
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   title: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   passcode: { type: String, required: true, minlength: 6 },
-  location: [
-    {
-      address: { type: String, required: true },
-      postcode: { type: String, required: true },
-      city: { type: String, required: true },
-      state: { type: String, required: true },
-      country: { type: String, required: true },
-    },
-  ],
+  location: { type: String, required: true },
+  isCompany: { type: Boolean, required: true },
   createdDate: { type: Date, default: Date.now },
 });
 userSchema.plugin(mongooseUniqueValidator);
