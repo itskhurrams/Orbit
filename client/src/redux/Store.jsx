90a127dc953fe52af399bdf371f2db86d25c0285
khurram shahzad <itskhurrams@outlook.com>
2022-05-02 21:35:38 +0500

    store

--- client/src/redux/Store.jsx
+++ client/src/redux/Store.jsx
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
-import rootReducer from '../reducers';
+import rootReducer from '../reducers/Index';
 
 const initialState = {};
 const middleWare = [thunk];
